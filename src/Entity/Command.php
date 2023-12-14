8a280092da5bb44f04d8753e4b5092cefac3c72b
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2023-12-14 08:11:33 +0100

    API: Resources added

--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -6,8 +6,10 @@ use App\Repository\CommandRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: CommandRepository::class)]
+#[ApiResource]
 class Command
 {
     #[ORM\Id]
