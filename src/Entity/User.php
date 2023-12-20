7cd122fcc9594d4fd84391aa2fbbbbf053e3ad3a
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2023-12-20 08:23:50 +0100

    REGISTER: Route added for register ('/register')

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte avec cet email.')]
 #[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
