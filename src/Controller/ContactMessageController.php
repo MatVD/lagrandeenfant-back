e96b91b7e8e50bcac6710b72b2f49587fc97d9a4
MatVD <vandammemathieu@hotmail.fr>
2025-08-04 17:02:43 +0200

    fix(contact): update ContactMessageController to extend AbstractController

--- src/Controller/ContactMessageController.php
+++ src/Controller/ContactMessageController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mailer\TwigMailer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
@@ -14,7 +14,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class ContactMessageController
+class ContactMessageController extends AbstractController
 {
     public function __invoke(
         Request $request,
